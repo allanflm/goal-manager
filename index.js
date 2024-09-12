c8b958e04418ef9ab7c1aa5cb0e01b6e4847d5ca
Allan <allanfelipedk@gmail.com>
2024-09-11 22:24:35 -0300

    feat: criando o menu

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const start = () => {
+const { select } = require("@inquirer/prompts");
+
+const start = async () => {
     while (true) {
-        let opcao = "sair";
+        const opcao = await select({
+            message: "Menu -> ",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar",
+                },
+                {
+                    name: "Sair",
+                    value: "sair",
+                },
+            ],
+        });
         switch (opcao) {
             case "cadastrar":
                 console.log("Vamos cadastrar!");
@@ -9,6 +23,7 @@ const start = () => {
                 console.log("Vamos listar!");
                 break;
             case "sair":
+                console.log("Até a proxima!");
                 return;
         }
     }
