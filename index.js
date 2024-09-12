dd6a2822c09096784b3304cbb9e0d90cda293550
Allan <allanfelipedk@gmail.com>
2024-09-11 22:16:36 -0300

    feat: add dependencie inquirer

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-// olá, mundo!
-console.log("olá, mundo!");
-
-// variáveis - let
-let mensagens = "olá, mundo!";
-
-// variáveis - const
-const mensagem = "olá, eu";
-{
-    const mensagem = "olá, Mayk!";
-    console.log(mensagem);
-}
-console.log(mensagem);
-
-// arrays
-let metas = ["mayk", "alô"];
-let metas01 = [2, "mayk"];
-// concatenando valores
-console.log(metas[1] + ", " + metas[0]);
-
-let metas02 = [
-    metas,
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked: false,
-    },
-];
-console.log(metas02[1].value);
-
-// objetos
-let meta = {
-    value: "ler um livro por mês",
-    address: 2,
-    checked: true,
-    isChecked: () => {
-        console.log(info);
-    },
+const start = () => {
+    while (true) {
+        let opcao = "sair";
+        switch (opcao) {
+            case "cadastrar":
+                console.log("Vamos cadastrar!");
+                break;
+            case "listar":
+                console.log("Vamos listar!");
+                break;
+            case "sair":
+                return;
+        }
+    }
 };
-console.log(meta.value);
 
-// function // arrow function
-const criarMeta = () => {};
-
-// named function
-function criarMetas() {}
+start();
