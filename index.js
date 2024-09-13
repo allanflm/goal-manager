9f2305bd4b1cc01e1e33aa6856c3db473bc75f0f
Allan <allanfelipedk@gmail.com>
2024-09-12 23:52:53 -0300

    feat: deletar metas

--- index.js
+++ index.js
@@ -75,6 +75,29 @@ const metasAbertas = async () => {
     });
 };
 
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false };
+    });
+
+    const itemsADeletar = await checkbox({
+        message: "Selecione item para deletar.",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    });
+    if (itemsADeletar.length == 0) {
+        console.log("Nenhum item a deletar.");
+        return;
+    }
+    itemsADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item;
+        });
+    });
+
+    console.log("Meta(s) deletada(s) com sucesso!");
+};
+
 const start = async () => {
     while (true) {
         const opcao = await select({
@@ -97,6 +120,10 @@ const start = async () => {
                     value: "abertas",
                 },
                 {
+                    name: "Deletar metas: ",
+                    value: "deletar",
+                },
+                {
                     name: "Sair.",
                     value: "sair",
                 },
@@ -116,6 +143,9 @@ const start = async () => {
                 break;
             case "abertas":
                 await metasAbertas();
+                break;
+            case "deletar":
+                await deletarMetas();
                 break;
             case "sair":
                 console.log("Até a próxima!");
